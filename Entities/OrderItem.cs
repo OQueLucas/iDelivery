a9dc132d12a96d69732e5dbbcfa44bfdddc57009
Lucas Queiroz <l_queiroz@outlook.com.br>
2023-06-28 17:24:50 -0300

    Inserting all entities to project

---
+++ Entities/OrderItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace iDelivery.Entities
+{
+    internal class OrderItem
+    {
+        Product Product { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public OrderItem() { }
+        public OrderItem(Product product, double price, int quantity)
+        {
+            Price = price;
+            Quantity = quantity;
+            Product = product;
+        }
+
+        public double SubTotal() {
+            return Quantity * Price;
+        }
+
+        public override string ToString()
+        {
+            return Product.Name
+                + ", $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Quantity: "
+                + Quantity
+                + ", Subtotal: $"
+                + SubTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,42 @@
-﻿using iDelivery.Entities;
+﻿using Enum.Entities;
+using iDelivery.Entities;
 using iDelivery.Entities.Enums;
+using System.Globalization;
 
-namespace iDelivery
+Console.WriteLine("Enter cliente data: ");
+Console.Write("Name: ");
+String clientName = Console.ReadLine(); // Alex Green
+Console.Write("Email: ");
+String email = Console.ReadLine(); // alex@gmail.com
+Console.Write("Birth date(DD / MM / YYYY): ");
+DateTime birthDate = DateTime.Parse(Console.ReadLine()); // 15 / 03 / 1985
+
+Console.WriteLine("Enter order data:");
+Console.Write("Status: ");
+OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine()); // Processing
+
+Client client = new(clientName, email, birthDate);
+Order order = new(DateTime.Now, status, client);
+
+Console.Write("How many items to this order? ");
+int amountItems = int.Parse(Console.ReadLine()); // 2
+
+for (int i = 1; i <= amountItems; i++)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Order order = new Order
-            {
-                Id = 1080,
-                Moment = DateTime.Now,
-                Status = OrderStatus.PendingPayment
-            };
-            Console.WriteLine(order);
-        }
-    }
+    Console.WriteLine($"Enter #{i} item data:");
+    Console.Write("Product name: ");
+    string productName = Console.ReadLine(); // TV / Mouse
+    Console.Write("Product price: ");
+    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); // 1000.00 / 40.00
+    Product product = new Product(productName, price);
+
+    Console.Write("Quantity: ");
+    int quantity = int.Parse(Console.ReadLine()); // 1 / 2
+    OrderItem orderItem = new OrderItem(product, price, quantity);
+
+    order.AddItem(orderItem);
 }
+
+
+Console.WriteLine();
+Console.WriteLine(order);
