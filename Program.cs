7174ad5bf6e8bcf4937a796956f645dcae68c4e8
OQueLucas <lucasestevam365@gmail.com>
2023-06-28 12:05:49 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using iDelivery.Entities;
+using iDelivery.Entities.Enums;
+
+namespace iDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+        }
+    }
+}
